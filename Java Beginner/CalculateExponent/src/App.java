6dcd7fda806817b9c49656b47a60cc013bd5a55f
Aykut Cihan Sevim <aykutcihansevim@gmail.com>
2022-02-27 00:17:43 +0300

    Java Beginner Projects Added

---
+++ Java Beginner/CalculateExponent/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+        /** @author Aykut Cihan Sevim - 2022 */
+    public static void main(String[] args) {
+        int s,u;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayıyı giriniz : ");
+        s = input.nextInt();
+        System.out.print("Üs olacak sayıyı giriniz : ");
+        u = input.nextInt();
+
+        int result = 1;
+
+        for(int i = 1; i<=u; i++)
+        {
+            result = result * s;
+        }
+        
+        System.out.println("Sonuç : " + result);
+    }
+}
